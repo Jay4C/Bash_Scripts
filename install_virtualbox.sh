98195679955eb7072c984df038efc1fc09982c89
Jason ALOYAU <jason.aloyau@outlook.fr>
2021-04-16 10:47:56 +0200

    install_virtualbox.sh

---
+++ install_virtualbox.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# follow these steps : https://vitux.com/how-to-install-virtualbox-on-ubuntu/
+
+sudo add-apt-repository multiverse && sudo apt-get update
+
+sudo apt install virtualbox
+
+virtualbox
+
+exit
