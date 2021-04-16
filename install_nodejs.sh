840963dc9f9fadf310e4e204f5dc975ddbdedb3d
Jason ALOYAU <jason.aloyau@outlook.fr>
2021-04-16 11:38:40 +0200

    install_nodejs.sh

---
+++ install_nodejs.sh
@@ -0,0 +1,11 @@
+#!/bin/bash 
+
+# follow these steps : https://www.digitalocean.com/community/tutorials/how-to-install-node-js-on-ubuntu-20-04
+
+sudo apt update
+
+sudo apt install nodejs
+
+nodejs -v
+
+sudo apt install npm
