8257f1d75e5e7538e194aacc62e4da4de21e5b9f
Jason ALOYAU <jason.aloyau@outlook.fr>
2021-04-18 11:45:10 +0200

    set_up_ssh_keys_ubuntu.sh

---
+++ set_up_ssh_keys_ubuntu.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# follow these steps : https://linuxize.com/post/how-to-set-up-ssh-keys-on-ubuntu-20-04/
+
+ls -l ~/.ssh/id_*.pub
+
+ssh-keygen -t rsa -b 4096 -C "your_email@domain.com"
+
+ls ~/.ssh/id_*
+
+ssh-copy-id remote_username@server_ip_address
+
+cat ~/.ssh/id_rsa.pub | ssh remote_username@server_ip_address "mkdir -p ~/.ssh && chmod 700 ~/.ssh && cat >> ~/.ssh/authorized_keys && chmod 600 ~/.ssh/authorized_keys"
+
+ssh remote_username@server_ip_address
+
+ssh sudo_user@server_ip_address
+
+sudo nano /etc/ssh/sshd_config
+
+sudo systemctl restart ssh
