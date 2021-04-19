a3b3c06b9e7a37998b30aeac37f83312d8f0f0f7
Jason ALOYAU <jason.aloyau@outlook.fr>
2021-04-19 10:38:20 +0200

    mount_an_exfat_drive_on_ubuntu.sh

---
+++ mount_an_exfat_drive_on_ubuntu.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# follow these steps : https://linuxize.com/post/how-to-mount-an-exfat-drive-on-ubuntu/
+
+sudo add-apt-repository universe
+
+sudo apt update 
+
+sudo apt install exfat-fuse exfat-utils
