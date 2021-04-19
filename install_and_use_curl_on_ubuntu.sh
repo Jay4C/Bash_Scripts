68e638c6fa56ad3dc4ce02405251ab6de817e35b
Jason ALOYAU <jason.aloyau@outlook.fr>
2021-04-19 13:37:28 +0200

    install_and_use_curl_on_ubuntu.sh

---
+++ install_and_use_curl_on_ubuntu.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# follow these steps : https://linuxize.com/post/how-to-install-and-use-curl-on-ubuntu-20-04/
+
+sudo apt update
+
+sudo apt install curl
+
+curl
+
+curl https://gnu.org
+
+curl -o linux.tar.xz https://cdn.kernel.org/pub/linux/kernel/v5.x/linux-5.0.5.tar.xz
+
+curl -O https://cdn.kernel.org/pub/linux/kernel/v5.x/linux-5.0.5.tar.xz
+
+curl -I https://www.gnu.org/
+
+curl -u FTP_USERNAME:FTP_PASSWORD ftp://ftp.example.com/file.tar.gz
