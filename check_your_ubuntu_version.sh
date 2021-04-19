472ea19214c1bfe40bfb31b5220d3b5765cefd80
Jason ALOYAU <jason.aloyau@outlook.fr>
2021-04-19 13:04:28 +0200

    check_your_ubuntu_version.sh

---
+++ check_your_ubuntu_version.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# follow these steps : https://linuxize.com/post/how-to-check-your-ubuntu-version/
+
+lsb_release -a
+
+lsb_release -d
+
+cat /etc/issue
+
+cat /etc/os-release
+
+hostnamectl
