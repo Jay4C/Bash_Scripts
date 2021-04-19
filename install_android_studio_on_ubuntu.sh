70b3d66e71f5c8f9935ea5586b8b2efd8d774f8f
Jason ALOYAU <jason.aloyau@outlook.fr>
2021-04-19 11:07:45 +0200

    install_android_studio_on_ubuntu.sh

---
+++ install_android_studio_on_ubuntu.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# follow these steps : https://linuxize.com/post/how-to-install-android-studio-on-ubuntu-18-04/
+
+sudo apt update
+
+sudo apt install openjdk-8-jdk
+
+java -version
+
+sudo snap install android-studio --classic
+
+android-studio
