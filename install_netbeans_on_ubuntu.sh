0a80e8d1a1eb8d81c5713ad9055b8a69a31006b9
Jason ALOYAU <jason.aloyau@outlook.fr>
2021-04-19 11:09:05 +0200

    install_netbeans_on_ubuntu.sh

---
+++ install_netbeans_on_ubuntu.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# follow these steps : https://linuxize.com/post/how-to-install-netbeans-on-ubuntu-18-04/
+
+sudo apt update
+
+sudo apt install openjdk-8-jdk
+
+java -version
+
+sudo snap install netbeans --classic
+
+netbeans
