abe643138ffca8d6406a416f1520d07aa3a68adf
Jason ALOYAU <jason.aloyau@outlook.fr>
2021-04-19 13:31:12 +0200

    upgrade_from_ubuntu_16_04_to_18_04.sh

---
+++ upgrade_from_ubuntu_16_04_to_18_04.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# follow these steps : https://linuxize.com/post/how-to-upgrade-to-ubuntu-18-04-bionic-beaver/
+
+sudo apt update
+
+sudo apt upgrade
+
+sudo apt dist-upgrade
+
+sudo apt install update-manager-core
+
+sudo do-release-upgrade
+
+lsb_release -a
