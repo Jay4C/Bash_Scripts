7848420dc37a6b93c163a332009b73e388067abc
Jason ALOYAU <jason.aloyau@outlook.fr>
2021-04-19 14:02:53 +0200

    install_and_configure_redis_ubuntu.sh

---
+++ install_and_configure_redis_ubuntu.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# follow these steps : https://linuxize.com/post/how-to-install-and-configure-redis-on-ubuntu-20-04/
+
+sudo apt update
+
+sudo apt install redis-server
+
+sudo systemctl status redis-server
+
+sudo nano /etc/redis/redis.conf
+
+nano /etc/redis/redis.conf
+
+sudo systemctl restart redis-server
+
+ss -an | grep 6379
+
+sudo ufw allow proto tcp from 192.168.121.0/24 to any port 6379
+
+redis-cli -h <REDIS_IP_ADDRESS> ping
