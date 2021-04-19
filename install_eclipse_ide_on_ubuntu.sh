f12edcd0c1eb74b8c1a77bd2ce75ac18f1267494
Jason ALOYAU <jason.aloyau@outlook.fr>
2021-04-19 12:45:21 +0200

    install_eclipse_ide_on_ubuntu.sh

---
+++ install_eclipse_ide_on_ubuntu.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# follow these steps : https://linuxize.com/post/how-to-install-the-latest-eclipse-ide-on-ubuntu-18-04/
+
+sudo apt install default-jre
+
+sudo snap install --classic eclipse
